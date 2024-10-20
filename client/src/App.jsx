2b83a51bd875f5038252c3f8459cf4b10f150ef4
Greeshma <morsugreeshmareddy@gmail.com>
2024-10-21 00:09:18 +0530

    Create the header section used flowbite,react-icons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 //import React from 'react'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/signin" element={<SignIn />}/>
